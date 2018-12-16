8488f162e302a4412a663b6994f374a347e94b4c
Jack <jackstephenson96@gmail.com>
2018-12-16 17:58:32 -0500

    added __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Initialize application
+app = Flask(__name__, static_folder=None)
+
+# app configuration
+app_settings = os.getenv(
+    'APP_SETTINGS',
+    'app.config.DevelopmentConfig'
+)
+app.config.from_object(app_settings)
+
+# Initialize Flask Sql Alchemy
+db = SQLAlchemy(app)
+
+# Import the application views
+from app import templates
+
